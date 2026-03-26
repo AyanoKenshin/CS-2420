175235d6def1ba773b9ad19070c063e1572ccad6
AyanoKenshin <rcramer1106@gmail.com>
2026-03-25 20:58:30 -0600

    BST done !

---
+++ BagADTwBST/Student.py
@@ -0,0 +1,20 @@
+class Student:
+
+    def __init__(self, first, last, ssn, email, age):
+        self.first = first
+        self.last = last
+        self.ssn = ssn
+        self.email = email
+        self.age = int(age)
+
+    def __eq__(self, rhs):
+        return self.ssn == rhs.ssn
+
+    def __lt__(self, rhs):
+        return self.ssn < rhs.ssn
+
+    def __gt__(self, rhs):
+        return self.ssn > rhs.ssn
+
+    def __str__(self):
+        return self.first + " " + self.last + " " + self.ssn + " " + self.email + " " + str(self.age)
